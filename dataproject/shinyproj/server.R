d63b3cf7e1b71d5bccad5359674bb24ab99501ea
Eudjinn <eugeneios@gmail.com>
2016-02-09 12:08:20 +0300

    second version

--- dataproject/shinyproj/server.R
+++ dataproject/shinyproj/server.R
@@ -1,35 +1,38 @@
 library(shiny)
 library(ggplot2)
+library(Quandl)
 
 shinyServer(function(input, output) {
 
     # regenerate the distribution only when its parameters change, 
     # not when any parameter of the histogram changes
-    x <- reactive({
-        rnorm(1000, mean = input$mean, input$sd)
+    oil <- reactive({
+        Quandl("DOE/RBRTE", 
+               trim_start=input$start, 
+               trim_end=input$end, 
+               type="raw", 
+               collapse=input$collapse, 
+               order = "asc")
     })
     
     output$thePlot <- renderPlot({
-        ggplot(data = NULL, aes(x())) + 
-        geom_histogram(binwidth = (max(x())-min(x()))/input$bins, 
-                       col = input$color, 
-                       aes(y = ..density.., 
-                           fill=..count..)) + 
-        geom_vline(aes(xintercept = input$mean), 
-                   color = input$color, 
-                   size = 1) +
-        geom_density(size = 1, 
-                     color = input$color) +
-        labs(title = "Normal random distribution", 
-             x = "Values", 
-             y = "Density")
+        qplot(data = oil(), x = Date, y = Value) + 
+            geom_line(color = input$color, 
+                      size = 1) +
+            labs(title = "Oil prices", 
+                 x = "Date", 
+                 y = "Value ($/barrel)")
     })
     output$color <- renderText({
         paste("Selected lines color code:", input$color)
     })
-    output$dist <- renderText({
-        sprintf("Normal distribution parameters: mean = %d, sd = %d", 
-                input$mean, 
-                input$sd)
+    output$color <- renderText({
+        paste("Dot period:", input$collapse)
+    })
+    output$start <- renderText({
+        paste("Selected start date:", input$start)
+    })
+    output$end <- renderText({
+        paste("Selected end date:", input$end)
     })
 })
--- dataproject/shinyproj/ui.R
+++ dataproject/shinyproj/ui.R
@@ -1,32 +1,42 @@
 library(shiny)
-library(ggplot2)
 
 shinyUI(fluidPage(
     
-    titlePanel("Course project - shiny application"),
+    titlePanel("Creating data products project: Oil prices"),
     
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        label = h3("Number of bins:"),
-                        min = 1,
-                        max = 50,
-                        value = 30),
             selectInput("color", 
-                        label = h3("Lines color:"), 
+                        label = h5("Select the color of the graph:"), 
                         choices = list("black" = 1, "red" = 2,
-                                       "green" = 3, "blue" = 4), selected = 3),
-            numericInput("mean", 
-                         label = h3("Mean:"), 
-                         value = 0),
-            numericInput("sd", 
-                         label = h3("Standard Diviation:"), 
-                         value = 1)),
-        mainPanel(
-            h2("Diagram parameters:"),
-            textOutput("color"),
-            textOutput("dist"),
-            plotOutput("thePlot")
+                                       "green" = 3, "blue" = 4), 
+                        selected = 3),
+            selectInput("collapse",
+                        label = h5("Select the time period of one dot of the graph:"),
+                        choices = list("daily" = "daily", 
+                                       "weekly" = "weekly", 
+                                       "monthly" = "monthly", 
+                                       "quarterly" = "quarterly", 
+                                       "annual" = "annual"),
+                        selected = "monthly"),
+            dateInput("start", 
+                      label = h5("Select the start date:"), 
+                      format = "yyyy-mm-dd",
+                      value = "2001-01-01",
+                      startview = "year"),
+            dateInput("end", 
+                      label = h5("Select the end date:"), 
+                      format = "yyyy-mm-dd",
+                      value = "2016-02-01",
+                      startview = "year"),
+            h6("If start/end date or time period represented by one dot has changed, the data for the graph will be reread and the graph will be redrawn."),
+            h6("If you change the color, the graph will be redrawn with selected color but the data will not be reread.")
+            ),
+            mainPanel(
+                h4("Selected graph parameters:"),
+                textOutput("color"),
+                textOutput("start"),
+                textOutput("end"),
+                plotOutput("thePlot"))
         )
-    )
 ))
